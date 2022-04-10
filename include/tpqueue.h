fda196ed81f2f00cbe337e142f313d2ff6125d84
Лапихин Александр <94218678+Al-Lapik@users.noreply.github.com>
2022-04-10 18:01:17 +0300

    B178 Лапихин Александр 21-ИСТ-5

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,54 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
-};
+ private:
+    T arr[size];
+    int first, last;
 
-struct SYM {
-  char ch;
-  int prior;
-};
+ public:
+    TPQueue() : first(0), last(0) {}
 
-#endif  // INCLUDE_TPQUEUE_H_
+    bool isFull() const {
+        return last - first >= size;
+    }
+
+    bool isEmpty() const {
+        return first == last;
+    }
+
+    void shift(int position) {
+        for (int i = (last - 1) % size; i > position; i--) {
+            if (arr[i - 1].ch != NULL) {
+                arr[i ] = arr[i - 1];
+            }
+        }
+    }
+
+    void push(T value) {
+        if (!isFull()) {
+            if (isEmpty()) {
+                arr[first % size] = value;
+            } else if (value.prior > arr[first % size].prior) {
+                shift(first % size);
+                arr[first % size] = value;
+            } else {
+                int i = first;
+                while (value.prior <= arr[i].prior)
+                    i++;
+                shift(i % size);
+                arr[i % size] = value;
+            }
+            last++;
+        } else {
+            throw std::string("Queue is full");
+        }
+    }
+
+    T pop() {
+        if (!isEmpty()) {
+            return arr[(first++) % size];
+        } else {
+            throw std::string("Queue is empty");
+        }
+    }
+};

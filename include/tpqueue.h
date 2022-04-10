85718319c1a10ce01535f85cd5748d1a6c39fcd5
Лапихин Александр <94218678+Al-Lapik@users.noreply.github.com>
2022-04-10 20:56:57 +0300

    Лапихин Александр 21-ИСТ-5

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -11,51 +11,22 @@ struct SYM {
 template<typename T, int size>
 class TPQueue {
  private:
-    T arr[100];
+    T arr[5];
     int first, last;
 
  public:
     TPQueue() : first(0), last(0) {}
 
-    bool isFull() const {
-        return last - first >= size;
-    }
-
-    bool isEmpty() const {
-        return first == last;
-    }
-
-    void shift(int position) {
-        for (int i = (last - 1) % size; i > position; i--)
-            arr[i] = arr[i - 1];
-    }
-
     void push(T value) {
-        if (!isFull()) {
-            if (isEmpty()) {
-                arr[first % size] = value;
-            } else if (value.prior > arr[first % size].prior) {
-                shift(first % size);
-                arr[first % size] = value;
-            } else {
-                int i = first;
-                while (value.prior <= arr[i].prior)
-                    i++;
-                shift(i % size);
-                arr[i % size] = value;
-            }
-            last++;
-        } else {
-            throw std::string("Queue is full");
-        }
+        int i = last;
+        while (--i >= first && value.prior > *(arr + i % size).prior)
+            *(arr + (i + 1) % size) = *(arr + i % size);
+        *(arr + (i + 1) % size) = value;
+        last++;
     }
 
     T pop() {
-        if (!isEmpty()) {
-            return arr[(first++) % size];
-        } else {
-            throw std::string("Queue is empty");
-        }
+        return *(arr + (first++) % size);
     }
 };
 

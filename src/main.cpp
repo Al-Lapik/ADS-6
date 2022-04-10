0115bb69338e46a257438e8994877cdf052885c2
Лапихин Александр <94218678+Al-Lapik@users.noreply.github.com>
2022-04-10 18:15:25 +0300

    B178 Лапихин Александр 21-ИСТ-5

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,5 @@
 #include "tpqueue.h"
 
 int main() {
-    TPQueue<SYM, 5> pqueue;
-    pqueue.push(SYM{'a', 4});
-    pqueue.push(SYM{'b', 7});
-    SYM c1 = pqueue.pop();
-    SYM c2 = pqueue.pop();
     return 0;
 }
